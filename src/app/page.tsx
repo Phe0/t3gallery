18679db5d18e57bfdc398ac1eca56ba91483c9d7
phe0 <pheofo@gmail.com>
2024-04-25 20:47:30 -0300

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/e1b56cbb-b984-4935-a164-9222b6ded502-8596p7.jpeg",
   "https://utfs.io/f/69c20ba8-40e1-4db9-8725-5f5ab31f5b90-ywqv8z.jpg",
@@ -9,12 +11,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image" />
           </div>
         ))}
