2039e3391eb5236b4947a29743d02e2df5406a86
phe0 <pheofo@gmail.com>
2024-04-25 20:09:16 -0300

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+const mockUrls = [
+  "https://utfs.io/f/e1b56cbb-b984-4935-a164-9222b6ded502-8596p7.jpeg",
+  "https://utfs.io/f/69c20ba8-40e1-4db9-8725-5f5ab31f5b90-ywqv8z.jpg",
+  "https://utfs.io/f/58af7902-2427-4c2a-9e26-8eb52f1617bb-hdf3fq.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
